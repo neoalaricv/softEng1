0882a40d1697db816b80b0c4a1f4e1d210236d28
neoalaricv <142380105+neoalaricv@users.noreply.github.com>
2023-08-16 14:26:14 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,1 +1,6 @@
-
+public class Hello{
+  public static void main(String[] args){
+    System.out.println("Hello, Software Engineering 1!");
+    System.out.println("Student Name: VILLANUEVA, Neo Alaric B.");
+    ;
+  ;
