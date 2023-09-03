d0b05ba15382f6219ca53bc4c97479b769aa1ef8
neoalaricv <neoalaric.villanueva@gmail.com>
2023-09-04 01:24:24 +0800

    Goodjob

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello{
     System.out.println("Hello, Software Engineering 1!");
     System.out.println("Student Name: VILLANUEVA, Neo Alaric B.");
     System.out.println("That is why developers are highly-paid!");
+    System.out.println ("Complicated world of SoftDev that is why it pays much!");
     ;
   ;
